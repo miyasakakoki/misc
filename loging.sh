b329f9f3ea3a4f5a05868f061e83e1995aabfcc5
miyasaka <mga4ttu@gmail.com>
2017-01-12 15:36:26 +0900

    ファイルの追加

---
+++ loging.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SITE="http://www.gaitameonline.com/rateaj/chartjson/USDJPY_i_bid.json"
+while sleep 3600; do
+	curl -s ${SITE} |
+		sed -n -e "s/\"currency[^,]*,//; s/yyyyMMddHHmm/time/g; 1d; \$d; /\]/d; p" |
+			sed -n -e " /`date +%Y%m%d%H%M -d"-1hour"`/,\$ p" >> `date +%Y`-1m.txt
+done
+
